27d563c2c46bc8c74468acdc00f3dc056a93c31d
Pooja <pooja.isabelle@gmail.com>
2020-10-19 17:50:51 +1100

    Completed welcome flask app route

--- app.py
+++ app.py
@@ -23,3 +23,14 @@ station = Base.classes.station
 # Flask Setup
 app = Flask(__name__)
 
+# Flask Routes
+@app.route("/")
+def welcome():
+    return (
+        f"Aloha! Welcome to the Honolulu, Hawai'i Climate Analysis Directory.<br/>"
+        f"You can explore the following climate analysis : <br/>"
+        f"1. Precipitation for the last 12 months: /api/v1.0/precipitation<br/>"
+        f"2. List of all stations: /api/v1.0/stations<br/>"
+        f"3. Temperature observations of the most active station for the last year of data: /api/v1.0/tobs<br/>"
+    )
+
